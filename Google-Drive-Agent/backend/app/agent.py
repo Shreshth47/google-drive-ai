07ebfe3fb766fa2cce4fa8239de8a1bf8b41fd16
Shreshth Shukla <162830928+Shreshth47@users.noreply.github.com>
2026-05-13 16:21:50 +0530

    Change model to 'gemini-2.5-flash-lite'

--- Google-Drive-Agent/backend/app/agent.py
+++ Google-Drive-Agent/backend/app/agent.py
@@ -9,7 +9,7 @@ from .tools import search_google_drive
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash", 
+    model="gemini-2.5-flash-lite", 
     temperature=0
 )
 tools = [search_google_drive]
